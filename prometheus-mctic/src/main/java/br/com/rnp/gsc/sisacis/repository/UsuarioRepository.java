0ca5ea0dfc9a1c912152eff6f631c8d6df70cc70
JHKroger00 <50018245+JHKroger00@users.noreply.github.com>
2020-05-24 09:48:00 -0300

    Delete UsuarioRepository.java

--- prometheus-mctic/src/main/java/br/com/rnp/gsc/sisacis/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.rnp.gsc.sisacis.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.rnp.gsc.sisacis.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-	public Usuario findById(long id);
-}
